81c7bd5ab53fd96245ac31da15532096d92bac60
Esraa Ragab <esragab@banquemisr.com>
2023-10-18 22:46:52 +0300

    Complete header view

--- Stocks/HeaderView.swift
+++ Stocks/HeaderView.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 50.0) {
+            HStack(alignment: .center, spacing: 20.0) {
+                Image(systemName: "apple.logo")
+                    .frame(width: 40, height: 40)
+                Text(/*@START_MENU_TOKEN@*/"Al Rajhi Capital"/*@END_MENU_TOKEN@*/)
+                    .multilineTextAlignment(.center)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                Image(systemName: "pencil")
+                    .foregroundColor(Color.black)
+                    .frame(width: 40, height: 40)
+            }
+            HStack(alignment: .center, spacing: 20.0) {
+                VStack {
+                    Text("Stocks")
+                        .padding(5)
+                    Text("12")
+                }
+                VStack {
+                    Text("Profit/Loss")
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .padding(5)
+                    HStack {
+                        Image(systemName: "arrow.up.square.fill")
+                            .foregroundColor(Color.black)
+                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 20))
+                        Text("13.56%")
+                            .font(.headline)
+                            .multilineTextAlignment(.center)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+            }
+        }
+        .padding(.all)
     }
 }
 
