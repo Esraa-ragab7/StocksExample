dff495d6df3b5bbbcdce14a72b648804ec40b5a1
Esraa Ragab <esragab@banquemisr.com>
2023-10-18 22:47:14 +0300

    Add dummy data

--- Stocks/DataModel.swift
+++ Stocks/DataModel.swift
@@ -7,6 +7,21 @@
 
 import UIKit
 
-class DataModel: NSObject {
+struct Stock: Hashable {
+    let id: Int
+    var title: String
+    var change: Double
+    var up: Bool
+}
 
+class DataModel: NSObject {
+    static let data: [Stock] = [
+        Stock(id: 1, title: "ARAMCO", change: +9.89, up: true),
+        Stock(id: 2, title: "ASEER", change: -10.0, up: false),
+        Stock(id: 3, title: "Al Alamiya", change: -2.69, up: false),
+        Stock(id: 4, title: "APC", change: +1.34, up: true),
+        Stock(id: 5, title: "STC", change: -2.42, up: false),
+        Stock(id: 6, title: "ZAIN KSA", change: +0.46, up: true),
+        Stock(id: 7, title: "TAPRCO", change: +2.2, up: true)
+    ]
 }
